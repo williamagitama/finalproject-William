2b45b14ab4753566db6e54a1affad233ae698b69
williamagitama <agitama.william@gmail.com>
2020-09-19 21:00:59 +0700

    Error message login

--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,5 @@
 import React from 'react'
-import './Header.css'
-import SearchIcon from '@material-ui/icons/Search';
-import LanguageIcon from '@material-ui/icons/Language';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import './Header.css';
 import { Avatar } from "@material-ui/core";
 import { Button } from "@material-ui/core";
 import { useHistory } from "react-router-dom";
@@ -16,15 +13,7 @@ function Header() {
                 <img className="header__icon" src="https://i.pinimg.com/originals/3c/bf/be/3cbfbe148597341fa56f2f87ade90956.png" alt="" />
             </Link>
 
-            <div className='header__center'>
-                <input type="text" />
-                    <SearchIcon />
-            </div>
-
             <div className='header__right'>
-                <p>Become a host</p>
-                <LanguageIcon />
-                <ExpandMoreIcon />
                 <Button onClick={() => history.push('/login')}><Avatar /></Button>
             </div>
 
--- src/LoginPage.js
+++ src/LoginPage.js
@@ -2,16 +2,45 @@ import React from 'react'
 import './LoginPage.css'
 import { Button } from "@material-ui/core";
 
-function LoginPage() {
-    return (
-        <div className='Login'>
-            <div className='user'>
-                <input type="text" placeholder="username"/>
-                <input type="password" placeholder="password"/>
-                <Button variant='outlined'>Login</Button>
+class LoginPage extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {
+            err: ''
+        };
+    }
+
+    login(e) {
+        e.preventDefault();
+        var username = e.target.elements.username.value;
+        var password = e.target.elements.password.value;
+        if(username == 'william' && password == '12345') {
+            this.setState({
+                err: 'Database tidak ditemukan'
+            });
+        }
+        else {
+            this.setState({
+                err: 'Username dan Password salah!'
+            });
+        }
+    }
+
+    render() {
+        return (
+            <div className='Login'>
+                <div className='user'>
+                {this.state.err != '' ? this.state.err : ''}
+                    <form method="post" onSubmit={this.login.bind(this)}>
+                        <input type="text" name="username" placeholder="username"/><br/>
+                        <input type="password" name="password" placeholder="password"/><br/>
+                        <Button variant='outlined' input type="submit">Login</Button>
+                    </form>
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default LoginPage
